210244c3759f61ea3d4337121b90aaee1041faa9
kkondo1981 <kkondo.odnokk@gmail.com>
2019-01-01 22:52:14 +0900

    add x_UD argument to aglm()

--- R/aglm.R
+++ R/aglm.R
@@ -1,11 +1,28 @@
 # fitting function for AGLM model
 # written by Kenji Kondo @ 2019/1/1
 
+
+#Inner function used in aglm()
+sliceByColWithoutDrop <- function(x, idx) {
+  z <- x[, idx]
+  if (is.null(dim(z))) {
+    z <- matrix(z, dim(x)[1])
+    names(z) <- names(x)[idx]
+  }
+  return(z)
+}
+
+
 #' fit an AGLM model
 #'
 #' @param x input matrix or data.frame of size (nobs, _). Each row shold be an integer or numeric vector.
-#' @param x_UD additional input matrix of size (nbos, _). Each row should be an integer or character or factor vector.
 #' @param y response variable.
+#' @param x_UD additional input matrix of size (nbos, _). optional. Each row of `x_UD` should be an integer,
+#'   character, or factor vector and this function replaces it with U-dummy matrix before fitting.
+#' @param UD_vars integer or character vector. If an integer vector is set, this function replaces columns whose
+#'   indices are in `UD_vars` into U-dummy matrices before inference. If a character vector is set, this function
+#'   replaces each column whose name is in `UD_vars` with U-dummy matrix before inference.
+#'   `UD_vars` is ignored when `x_UD` is not NULL.
 #' @param family response type. Currently gaussian, binomial, and poisson are supported.
 #' @param standardize logical flag for x variable standardization, prior to fitting the model sequence.
 #'   Note that aglm() standardizes only x and never x_UD.
@@ -16,7 +33,8 @@
 #' @export
 #' @importFrom assertthat assert_that
 #' @importFrom glmnet glmnet
-aglm <- function(x, x_UD, y,
+aglm <- function(x, y, x_UD=NULL,
+                 UD_vars=NULL,
                  family=c("gaussian","binomial","poisson"),
                  weights,
                  offset=NULL,
@@ -39,16 +57,43 @@ aglm <- function(x, x_UD, y,
                  standardize.response=FALSE) {
   # Check and process arguments.
   assert_that(class(x) == "matrix" | class(x) == "data.frame")
-  assert_that(class(x_UD) == "matrix" | class(x_UD) == "data.frame")
-  assert_that(dim(x)[1] == dim(x_UD)[1])
+  if (!is.null(x_UD)) {
+    assert_that(class(x_UD) == "matrix" | class(x_UD) == "data.frame")
+    assert_that(dim(x)[1] == dim(x_UD)[1])
+  }
 
   y <- drop(y)
   assert_that(class(y) == "integer" | class(y) == "numeric")
   assert_that(length(y) == dim(x)[1])
 
-  n <- dim(x)[1]
-  nvar <- dim(x)[2] + dim(x_UD)[2]
-  nvar_UD <- dim(x_UD)[2]
+  nobs <- length(y)
+
+
+  # Create x_UD from UD_vars if UD_vars is given
+  if (!is.null(UD_vars)) {
+    assert_that(is.null(x_UD))
+    if (is.integer(UD_vars) | is.numeric(UD_vars)) {
+      UD_vars <- as.integer(UD_vars)
+      x_UD <- sliceByColWithoutDrop(x, UD_vars)
+      x <- sliceByColWithoutDrop(x, -UD_vars)
+    } else {
+      var_names <- colnames(x)
+      assert_that(!is.null(var_names))
+
+      x_UD <- sliceByColWithoutDrop(x, var_names %in% UD_vars)
+      x <- sliceByColWithoutDrop(x, !(var_names %in% UD_vars))
+    }
+  }
+
+
+  # Number of variables
+  nvar <- 0
+  if (!is.null(x)) nvar <- dim(x)[2]
+  if (!is.null(x_UD)) {
+    nvar_UD <- dim(x_UD)[2]
+    nvar <- nvar + nvar_UD
+  }
+  assert_that(nvar > 0)
 
 
   # Create a variable information list and design matrix passed to glmnet
@@ -56,31 +101,35 @@ aglm <- function(x, x_UD, y,
   x_for_glmnet <- NULL
 
   # Get O-dummies for each column of x
-  for (i in seq(nvar - nvar_UD)) {
-    res <- getODummyMatForOneVec(x[, i])
+  if (nvar - nvar_UD > 0) {
+    for (i in seq(nvar - nvar_UD)) {
+      res <- getODummyMatForOneVec(x[, i])
 
-    var_name <- names(x)[i]
-    if (is.null(var_name)) var_name <- paste0("X_", i)
-    var_info <- list(name=var_name,
-                     type="O",
-                     breaks=res$breaks)
-    vars_info[[i]] <- var_info
+      var_name <- names(x)[i]
+      if (is.null(var_name)) var_name <- paste0("X_", i)
+      var_info <- list(name=var_name,
+                       type="O",
+                       breaks=res$breaks)
+      vars_info[[i]] <- var_info
 
-    x_for_glmnet <- cbind(x_for_glmnet, res$dummy_mat)
+      x_for_glmnet <- cbind(x_for_glmnet, res$dummy_mat)
+    }
   }
 
   # Get U-dummies for each column of x_UD
-  for (i in seq(nvar_UD)) {
-    res <- getUDummyMatForOneVec(x_UD[, i])
+  if (nvar_UD > 0) {
+    for (i in seq(nvar_UD)) {
+      res <- getUDummyMatForOneVec(x_UD[, i])
 
-    var_name <- names(x_UD)[i]
-    if (is.null(var_name)) var_name <- paste0("X_UD_", i)
-    var_info <- list(name=var_name,
-                     type="U",
-                     levels=res$levels)
-    vars_info[[nvar - nvar_UD + i]] <- var_info
+      var_name <- names(x_UD)[i]
+      if (is.null(var_name)) var_name <- paste0("X_UD_", i)
+      var_info <- list(name=var_name,
+                       type="U",
+                       levels=res$levels)
+      vars_info[[nvar - nvar_UD + i]] <- var_info
 
-    x_for_glmnet <- cbind(x_for_glmnet, res$dummy_mat)
+      x_for_glmnet <- cbind(x_for_glmnet, res$dummy_mat)
+    }
   }
 
 
