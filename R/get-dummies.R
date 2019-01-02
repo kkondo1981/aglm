9cb32ca1556014180c6359bb4a585b305b530bbb
kkondo1981 <kkondo.odnokk@gmail.com>
2019-01-02 17:21:01 +0900

    add levels argument to getUDummyMatForOneVec and modify comments of get-dummies.R

--- R/get-dummies.R
+++ R/get-dummies.R
@@ -3,26 +3,30 @@
 
 #' Get U-dummy matrix for one-dimensional vector
 #'
-#' @param x_vec non-numeric vector to be converted into dummy matrix.
-#' @param drop_last boolean value. If TRUE, the last column of dummy matrix is dropped to avoid colinear
-#' @param only_info boolean value. If TRUE, actual creation of dummy matrix is omitted.
+#' @param x_vec A non-numeric vector to be converted into dummy matrix.
+#' @param levels A character vector indicates unique set of possible values.
+#'   If NULL, all the unique values of `x_vec` are used.
+#' @param drop_last A boolean value. If TRUE, the last column of dummy matrix is dropped to avoid colinear
+#' @param only_info A bboolean value. If TRUE, actual creation of dummy matrix is omitted.
 #'
 #' @return a list with two members `levels` and `dummy_mat`.
-#' * `levels`: a character vector. unique set of possible values. sorted.
-#' * `drop_last`: same as input
-#' * `dummy_mat`: an integer matrix with size (length of `x_vec`, length of `levels` or minus 1 when `drop_last=TRUE`).
+#' * `levels`: Same as input
+#' * `drop_last`: Same as input
+#' * `dummy_mat`: An integer matrix with size (length of `x_vec`, length of `levels` or minus 1 when `drop_last=TRUE`).
 #'   `dummy_mat[i, j]` is 1 if and only if `x_vec[i] == levels[j]`, and 0 otherwise.
 #'   Omitted if `only_info=TRUE`
 #'
 #' @export
 #' @importFrom assertthat assert_that
-getUDummyMatForOneVec <- function(x_vec, drop_last=TRUE, only_info=FALSE) {
+getUDummyMatForOneVec <- function(x_vec, levels=NULL, drop_last=TRUE, only_info=FALSE) {
   # Check arguments. numerical vectors are not allowed.
   assert_that(class(x_vec) == "integer" | class(x_vec) == "character" | class(x_vec) == "factor")
 
-  # Convert x_vec into factor if not
-  if (class(x_vec) != "factor") {
-    x_vec <- as.factor(x_vec)
+  # Create factor. Note that if x_vec is itself factor and levels is not specified, do not need nothing.
+  if (!is.factor(x_vec) | !is.null(levels)) {
+    x_vec <- as.character(x_vec)
+    if (is.null(levels)) x_vec <- factor(x_vec)
+    else x_vec <- factor(x_vec, levels=levels)
   }
 
   # create dummy matrix for x_vec
@@ -39,15 +43,15 @@ getUDummyMatForOneVec <- function(x_vec, drop_last=TRUE, only_info=FALSE) {
 
 #' Get O-dummy matrix for one-dimensional vector
 #'
-#' @param x_vec integer or numeric vector to be converted into dummy matrix.
-#' @param breaks a numeric vector which indicates the boundaries of bins, of length (number of bins + 1).
-#'   If NULL is set, evenly cut bins are generated and used.
-#' @param nbin a number of bins which is used. Only used when `breaks` is not set.
-#' @param only_info boolean value. If TRUE, actual creation of dummy matrix is omitted.
+#' @param x_vec An integer or numeric vector to be converted into dummy matrix.
+#' @param breaks A numeric vector which indicates the boundaries of bins, of length (number of bins + 1).
+#'   If NULL, evenly cut bins are automatically generated and used.
+#' @param nbin A number of bins which is used. Only used when `breaks` is not set.
+#' @param only_info A boolean value. If TRUE, actual creation of dummy matrix is omitted.
 #'
 #' @return a list with two members `breaks` and `dummy_mat`.
-#' * `breaks`: a numeric vector which indicates the boundaries of bins, of length (number of bins + 1).
-#' * `dummy_mat`: an integer matrix with size (length of `x_vec`, length of `breaks` minus 1).
+#' * `breaks`: Same as input
+#' * `dummy_mat`: An integer matrix with size (length of `x_vec`, length of `breaks` minus 1).
 #'   `dummy_mat[i, j]` is 1 if and only if `breaks[i] < x_vec[i] <= breaks[i+1]`, and 0 otherwise.
 #'   Note that, in case where `x_vec[i]` is outside of `(breaks[1], breaks[length(breaks)]]`,
 #'   `x_vec[i]` is considered to be in the first bin if `x_vec[i] <= breaks[1]`, and
--- tests/testthat/test_get-dummies.R
+++ tests/testthat/test_get-dummies.R
@@ -5,6 +5,7 @@ test_that("getUDummyMatForOneVec()'s outputs are correct.", {
   expect_equal(getUDummyMatForOneVec(1:3)$dummy_mat, matrix(c(1, 0, 0, 0, 1, 0), 3, 2))
   expect_equal(getUDummyMatForOneVec(1:3, drop_last=FALSE)$dummy_mat, matrix(c(1, 0, 0, 0, 1, 0, 0, 0, 1), 3, 3))
   expect_equal(getUDummyMatForOneVec(c("a", "b", "b", "e", "d", "f"))$levels, c("a", "b", "d", "e", "f"))
+  expect_equal(getUDummyMatForOneVec(c("a", "b", "b", "e", "d", "f"), levels=c("a", "b", "c", "d", "e", "f"))$levels, c("a", "b", "c", "d", "e", "f"))
 })
 
 test_that("getODummyMatForOneVec()'s outputs are correct.", {
