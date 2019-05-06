c0a6da8bffb79e9a44e42c7b281cceffe99c1fbb
kkondo1981 <kkondo.odnokk@gmail.com>
2019-05-06 20:22:04 +0900

    Added `s` option for `plot.AccurateGLM()`.

--- R/plot-aglm.R
+++ R/plot-aglm.R
@@ -5,10 +5,14 @@
 #'
 #' @param model An AccurateGLM object.
 #' @param verbose If TRUE, outputs details.
+#' @param s A numeric value specifying lambda value at which plotting is required.
+#'   Note that this function can't plot for multiple lambda values, so it allows only
+#'   single `s` value (which means `model` is trained with multiple lambda values and plot with one of them),
+#'   or `s=NULL` (which means `model` is trained with single lambda value and plot with that value).
 #'
 #' @export
-plot.AccurateGLM <- function(model, verbose=TRUE, ...) {
-  coefs_all <- coef(model)
+plot.AccurateGLM <- function(model, verbose=TRUE, s=NULL, ...) {
+  coefs_all <- coef(model, s=s)
   nvars <- length(model@vars_info)
 
   devAskNewPage(TRUE)
@@ -16,7 +20,7 @@ plot.AccurateGLM <- function(model, verbose=TRUE, ...) {
     var_info <- model@vars_info[[i]]
     if (var_info$type == "inter") break ## no plot for interactions
 
-    coefs <- coef(model, index=var_info$idx)
+    coefs <- coef(model, index=var_info$idx, s=s)
     main <- sprintf("Cofficients Plot for variable `%s`", var_info$name)
 
     if (var_info$type == "quan") {
